25cce85a153cea500d6ddf9c253af6dd667fa153
Agentscreech <travis.walentin@gmail.com>
2017-02-05 01:22:42 -0800

    materialize update, parital

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("JS WORKING");
+$(document).ready(function() {
+     $('.scrollspy').scrollSpy();
+      $(".button-collapse").sideNav({
+          edge: 'right',
+          menuWidth: 150,
+      });
+    $('#revealcpu').hover(
+        function() {
+            $('#oldcpu').fadeIn();
+        },
+        function() {
+            $('#oldcpu').fadeOut('slow');
+        }
+    );
+});
